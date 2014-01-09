281fb5e6fdb0c0e1a928256a84497b4498e65847
Borja Sotomayor <borja@cs.uchicago.edu>
2014-01-09 14:08:08 -0600

    Fixed: Trivial p1a solution didn't work with port randomizer

--- samples/chirc/project1a-trivial.c
+++ samples/chirc/project1a-trivial.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -15,11 +16,27 @@ int main(int argc, char *argv[])
 	int yes = 1;
 	socklen_t sinSize = sizeof(struct sockaddr_in);
 
+	int opt;
+	char *port = "6667";
+
+	while ((opt = getopt(argc, argv, "p:o:h")) != -1)
+		switch (opt)
+		{
+			case 'p':
+				port = strdup(optarg);
+				break;
+			case 'o':
+				break;
+			default:
+				printf("ERROR: Unknown option -%c\n", opt);
+				exit(-1);
+		}
+
 	char *msg = ":bar.example.com 001 user1 :Welcome to the Internet Relay Network user1!user1@foo.example.com\r\n";
 
 	memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(7776);
+	serverAddr.sin_port = htons(atoi(port));
 	serverAddr.sin_addr.s_addr = INADDR_ANY;
 	
 	serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
