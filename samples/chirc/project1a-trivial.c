8fb3d65beca3a945a03d0974afa39b7c0bffc5ef
Borja Sotomayor <borja@cs.uchicago.edu>
2016-01-05 14:41:31 -0600

    Updated trivial p1a implementation

--- samples/chirc/project1a-trivial.c
+++ samples/chirc/project1a-trivial.c
@@ -1,36 +1,83 @@
+/*
+ *
+ *  CMSC 23300 / 33300 - Networks and Distributed Systems
+ *
+ *  main() code for chirc project
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#include "log.h"
+
 int main(int argc, char *argv[])
 {
+    int opt;
+    char *port = "6667", *passwd = NULL;
+    int verbosity = 0;
+
+    while ((opt = getopt(argc, argv, "p:o:vqh")) != -1)
+        switch (opt)
+        {
+        case 'p':
+            port = strdup(optarg);
+            break;
+        case 'o':
+            passwd = strdup(optarg);
+            break;
+        case 'v':
+            verbosity++;
+            break;
+        case 'q':
+            verbosity = -1;
+            break;
+        case 'h':
+            fprintf(stderr, "Usage: chirc -o PASSWD [-p PORT] [(-q|-v|-vv)]\n");
+            exit(0);
+            break;
+        default:
+            fprintf(stderr, "ERROR: Unknown option -%c\n", opt);
+            exit(-1);
+        }
+
+    if (!passwd)
+    {
+        fprintf(stderr, "ERROR: You must specify an operator password\n");
+        exit(-1);
+    }
+
+    /* Set logging level based on verbosity */
+    switch(verbosity)
+    {
+    case -1:
+        chirc_setloglevel(QUIET);
+        break;
+    case 0:
+        chirc_setloglevel(INFO);
+        break;
+    case 1:
+        chirc_setloglevel(DEBUG);
+        break;
+    case 2:
+        chirc_setloglevel(TRACE);
+        break;
+    default:
+        chirc_setloglevel(INFO);
+        break;
+    }
+
 	int serverSocket;
 	int clientSocket;
 	struct sockaddr_in serverAddr, clientAddr;
 	int yes = 1;
 	socklen_t sinSize = sizeof(struct sockaddr_in);
-
-	int opt;
-	char *port = "6667";
-
-	while ((opt = getopt(argc, argv, "p:o:h")) != -1)
-		switch (opt)
-		{
-			case 'p':
-				port = strdup(optarg);
-				break;
-			case 'o':
-				break;
-			default:
-				printf("ERROR: Unknown option -%c\n", opt);
-				exit(-1);
-		}
 
 	char *msg = ":bar.example.com 001 user1 :Welcome to the Internet Relay Network user1!user1@foo.example.com\r\n";
 
